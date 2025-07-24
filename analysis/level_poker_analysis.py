0febe425e87bcba0bdd2ba78db52411b457f0064
cuboids <44651818+cuboids@users.noreply.github.com>
2025-07-24 13:50:09 +0200

    Create level_poker_analysis.py
    Add some questions we would like to analyze

---
+++ analysis/level_poker_analysis.py
@@ -0,0 +1,16 @@
+"""
+
+Here we try to answer questions like:
+
+- What is the distribution of deck levels?
+- What is the distribution of board levels?
+- What is the distribution of NLH hand levels?
+- How often do we make what on showdown?
+
+And we will perform simulations to determine:
+
+- At a constant deck level, what percentile is each
+NLH starting hand if we rank them in terms of all-in
+equity vs. one opponent with any two cards?
+
+"""
