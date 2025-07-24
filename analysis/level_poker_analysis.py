58bc1e62217518c4aa11595e8f34c22f7f0ebbfa
cuboids <44651818+cuboids@users.noreply.github.com>
2025-07-24 15:02:36 +0200

    Update level_poker_analysis.py

--- analysis/level_poker_analysis.py
+++ analysis/level_poker_analysis.py
@@ -9,4 +9,56 @@ Here we try to answer questions like:
 - %ile ranking for starting hands in Level N deck
 - %ile ranking for showdown categories in Level N deck
 
+Opening hand chart:
+
+Each deck has four cards per rank. In terms of level,
+we can sort them Top, Upper, Lower, Bottom.
+
+# # # # # # # # # # #
+# Deck level: 6     #
+# Best hand:        #
+#                   #
+# Xs Yd Zh Ah Bh    #
+# # # # # # # # # # #
+
+# # # # # # # # # # #
+# Hand: [AA]        #
+# Levels: 6432      #
+#                   #
+# tu% tl% tb%       #
+#     ul% ub%       #
+#         lb%       #
+# # # # # # # # # # #
+
+# # # # # # # # # # #
+# Hand: [AKs]       #
+# Levels: 6432+3254 #
+#                   #
+# t% u% l% b%       #
+# # # # # # # # # # #
+
+# # # # # # # # # # #
+# Hand: [AKo]       #
+# Levels 6432*5432  #
+#                   #
+# tt% tu% tl% tb%   #
+# ut% uu% ul% ub%   #
+# lt% lu% ll% lb%   #
+# bt% bu% bl% bb%   #
+# # # # # # # # # # #
+
+If levels
+are tied, we gray out the corresponding indicators. E.g.
+if all aces are Level 2, we only have a top and upper 
+ace.
+
+# # # # # # # # # # #
+# Hand: [AA]        #
+# Levels: 2         #  
+#                   #
+# tu% t̶l̶%̶ t̶l̶%̶       #
+#     t̶l̶%̶ t̶l̶%̶       #
+#         t̶l̶%̶       #
+# # # # # # # # # # #
+
 """
