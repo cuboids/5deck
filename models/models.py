9427b27c7e434f2f4f5bf260d1b545546012f4dc
cuboids <44651818+cuboids@users.noreply.github.com>
2025-07-24 13:38:14 +0200

    Create models.py
    Add partial description of models

---
+++ models/models.py
@@ -0,0 +1,137 @@
+""" Models 
+
+# Some preliminary models below.
+
+# suitable for:
+
+# SD (short deck)
+# 5D (five deck)
+# LP (level poker)
+# IP (infinite poker)
+
+# rank
+
+:class Rank(IntEnum)
+
+:class ShortRank(Rank)  # ranks 6-Ace for short deck
+
+# suit
+
+:class Suit(StrEnum)
+
+# level
+
+:class Level(IntEnum)
+
+:class InfLevel(Level)  # allows arbitrariliy high levels
+
+:class ShortLevel(Level)  # levels 6-14 only for short deck
+
+# card
+
+:class Card(StrEnum)  # a card has rank, suit, and level | None
+
+# deck
+
+:class Deck
+
+:class ShortDeck(Deck)  # a short deck with 36 cards: 6-Ace
+
+:class InfDeck(Deck)  # a deck with infinitely many cards, with InfLevels
+
+# hand
+
+:class Hand
+
+:class OmahaHand(Hand)  # a hand with four playing cards
+
+:class HoldEmHand(Hand)  # a hand with two playing cards
+
+# board
+
+:class Board  # a board with 0-5 x 1-3 = 0-15 playing cards
+# 1-3: run it once, twice or thrice.
+
+# game
+
+:class Game
+
+:class PLO(Game)
+
+:class FiveDeckPLO(PLO)
+
+:class NLH(Game)
+
+:class LevelNLH(NLH)
+
+# Later we might add FiveDeckNLH, LevelPLO, InfNLH, and InfPLO
+# as well as short deck variants with 5 decks, levels, or both.
+
+# player
+
+:class Player
+
+:class AIPlayer(Player)  # for AI to be a player
+
+# chips
+
+:class Chips
+
+# pot
+
+:class Pot
+
+# round
+
+:class Round  # an entire round, from preflop to showdown
+
+# hand evaluation
+
+:func hand_evaluation
+
+# each hand has a level, tier, category, groups, and level groups,
+# in that order of importance.
+#
+# For example: As(3), Kc(2), 9c(2), 7d(4), 2h(2) has
+#    - Level = min(3, 2, 2, 4, 2) = 2
+#    - Tier = Bronze (1/5)
+#    - Category = High Card (1/5 in Tier)
+#    - Groups = [Ace, King, 9, 7, deuce]
+#    - Level Groups = [3, 2, 2, 4, 2]
+#
+# Hand evaluation should work on:
+#    - A list of one or more cards
+#    - A board + hand
+# but not on:
+#    - A board + multiple hands.
+# for that we use the :func distribute_pot
+
+:class HandLevel
+
+:class HandTier
+
+:class HandCategory
+
+:class HandGroup1
+
+:class HandGroup2
+
+:class HandGroup3
+
+:class HandGroup4
+
+:class HandGroup5
+
+:class HandLevelGroup1
+
+:class HandLevelGroup2
+
+:class HandLevelGroup3
+
+:class HandLevelGroup4
+
+:class HandLevelGroup5
+
+:func distribute_pot
+
+"""
